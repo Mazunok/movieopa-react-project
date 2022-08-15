3372b625523e84ddb648b2917352b44ef75afef1
Mazunok <93519242+Mazunok@users.noreply.github.com>
2022-08-15 19:48:56 +0300

    refactor(movieopa_project): add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { GlobalStyles } from "./ui/globalStyles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <GlobalStyles />
     <App />
   </BrowserRouter>
 );
---
+++ src/ui/globalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding:0;
+}
+
+body{
+    font-family:Arial, Helvetica, sans-serif;
+}
+
+a{
+    text-decoration:none;
+}
+`;
