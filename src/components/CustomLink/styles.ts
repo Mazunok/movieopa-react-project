2c31a796cb740ffe25b67f7e38ee05799951f8db
Mazunok <93519242+Mazunok@users.noreply.github.com>
2022-09-17 19:27:03 +0300

    refactor(movieopa_project): make custom links active

--- src/components/CustomLink/styles.ts
+++ src/components/CustomLink/styles.ts
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Color } from "../../ui/colors";
 import { S1 } from "../../ui/typography";
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(NavLink)`
   display: flex;
   flex-direction: row;
   ${S1};
   color: ${Color.Secondary};
-  :active {
+  text-decoration: none;
+  &.active {
     color: ${Color.Primary};
   }
 `;
--- src/components/MainTemplate/MainTemplate.tsx
+++ src/components/MainTemplate/MainTemplate.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import { useToggle } from "hooks";
 import { Outlet } from "react-router-dom";
 import { Header } from "../Header/Header";
 import { Nav } from "../Nav/Nav";
 import { Container, Wrapper } from "./styles";
 
 export const MainTemplate = () => {
+  const [isNavOpen, setIsNavOpen] = useToggle(false);
   return (
     <Wrapper>
-      <Header />
+      <Header isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
       <Container>
-        <Nav />
+        <Nav isNavOpen={isNavOpen} />
         <Outlet />
       </Container>
     </Wrapper>
